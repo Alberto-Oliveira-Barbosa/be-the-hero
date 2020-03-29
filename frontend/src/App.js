e13b0cd5aa7f2ab3c2fcb968e5d5e4d48c22d32d
Alberto Oliveira Barbosa <albertobarbosa115@gmail.com>
2020-03-28 23:35:31 -0300

    Add back-end

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Ok, não dá pra viver só com python...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Então vamos pra React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
